0927d2a342e56a42aa52204f1df41c98e5f5ebd4
Tom Sproull <64221818+sprouller@users.noreply.github.com>
2023-03-22 00:13:12 +0000

    initial commit

---
+++ api-test.js
@@ -0,0 +1,17 @@
+
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+
+    }};
+  
+  fetch('https://api.marketstack.com/v1/eod?access_key=87d00fdf3f20582f0814ef0f74b55518&symbols=AAPL', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    console.log(response);
